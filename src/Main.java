958cdabd289288a4a6e3b6cdc60ae17bdeb86969
anlin <hello@gmail.com>
2022-08-19 11:30:03 +0300

    Homework 3.

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        int a = 10;
+        {
+            while (a <= 37)
+            {
+                System.out.println(a);
+                a = a + 3;
+            }
+        }
+
+    }
+
+}
